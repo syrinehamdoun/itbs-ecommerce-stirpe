44ac8d2a2b67e6e0af831077305e96ecdcf51edf
Syrine Hamdoun <hamdoun.syrine@gmail.com>
2022-11-29 21:46:35 +0100

    first commit

---
+++ backend/products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "http://localhost:5000/iphone12pro.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "http://localhost:5000/iphone12.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "http://localhost:5000/galaxyS.png",
+  },
+];
+
+module.exports = products;
